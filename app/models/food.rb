5c0ba17d8441838c8d91e12e7daf0a03f7612f8a
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-06 15:23:33 +0300

    Initailiz Linters

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_many   :recipe_food
+  has_many :recipe_food
 
-  validates :name, presence: true, length: {minimum:3, maximum:15}
-
+  validates :name, presence: true, length: { minimum: 3, maximum: 15 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 
-  validates :name , presence: true, length: {minimum:3, maximum:15}
+  validates :name, presence: true, length: { minimum: 3, maximum: 15 }
 end
