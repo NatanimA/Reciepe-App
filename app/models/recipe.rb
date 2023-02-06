349de5f3887efa0aee38a79b4542405bd38df1e8
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-06 15:09:30 +0300

    Insert Recipe Model and Validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   has_many :recipe_food
-  belongs_to :user, class_name: "user"
+  belongs_to :user
 
   validates :name, presence: true, length: {minimum:3, maximum:15}
   validates :description, presence: true, length: {minimum:3, maximum:200}
