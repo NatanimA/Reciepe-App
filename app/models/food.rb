baab37559e3bc919d39a8359a6d159877097f3d2
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-06 15:09:05 +0300

    Insert Food Model and validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
-  belongs_to :user, class_name: "user"
+  belongs_to :user
   has_many   :recipe_food
 
   validates :name, presence: true, length: {minimum:3, maximum:15}
