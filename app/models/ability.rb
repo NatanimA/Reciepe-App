8e15382a11fc8ca69ff152ffbf912ec43a09bb36
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-08 16:52:38 +0300

    Cancan Ability configurations

---
+++ app/models/ability.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can %i[create read show public shoping], Recipe
+    can %i[create read show], Food
+    can :delete, Recipe, user_id: user.id
+    can :delete, Food, user_id:user.id
+  end
+end
