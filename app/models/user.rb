2eb1ddb97b2f691bbb34419a6cbdd9a93f6e7ba0
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-08 16:52:52 +0300

    Devise Helpers Insertions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 
-  validates :name, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 40 }
 end
